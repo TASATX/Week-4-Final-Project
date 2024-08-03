c511b4a9ba3fefcac31a05d32cb09357f613004f
Tim <tas57atx@gmail.com>
2024-08-03 13:42:44 -0500

    updated to include all screen sizes

---
+++ app 1.js
@@ -0,0 +1,16 @@
+//  curl "https://retoolapi.dev/t7khno/data"
+
+
+/*
+let State = "NY";
+let City = "New York";
+let RegionName = "";
+let RegionName = ""; 
+async function fetchData() {
+  try {
+@@ -29,5 +29,5 @@ async function fetchData() {
+  }
+}
+fetchData();
+fetchData();  */
+clearIntervalgit status
